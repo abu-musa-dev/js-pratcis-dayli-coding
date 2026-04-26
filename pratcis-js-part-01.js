9a31726cc79ffa5743c7f8bb8898d363c864bbf2
abumusa8 <abumusa0740@gmail.com>
2026-04-26 21:41:06 +0600

    add function

--- pratcis-js-part-01.js
+++ pratcis-js-part-01.js
@@ -1,9 +1,25 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then(res => {
-    if(!res.ok){
-       throw new Error('data network')
-    }
-    return res.json()
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(res => {
+//     if(!res.ok){
+//        throw new Error('data network')
+//     }
+//     return res.json()
+//   })
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error));
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res =>res.json() )
+.then(data => {
+    console.log(data)
+    const container= document.getElementById('content')
+
+        data.forEach (user =>{
+        console.log(user)
+        const div = document.createElement('div')
+        div.innerText = user.name
+        div.style.border = '1px solid black'
+        container.appendChild(div)
+    })
+})
+
