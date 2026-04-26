04eabfe202a771249eea86f01e94884c37ee78a3
abumusa8 <abumusa0740@gmail.com>
2026-04-26 20:23:30 +0600

    add here

--- pratcis-js-part-01.js
+++ pratcis-js-part-01.js
@@ -1,3 +1,9 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(res => res.json())
-.then(data => console.log(data))
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(res => {
+    if(!res.ok){
+       throw new Error('data network')
+    }
+    return res.json()
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
